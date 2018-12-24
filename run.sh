389ceb0286b9ee16ddd3346cf39356c2c726adc7
Håvard Ottestad <hmottestad@gmail.com>
2018-12-24 14:50:09 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mvn clean install
+java -jar target/signature-problems-1.0-SNAPSHOT.jar
+
---
+++ src/main/java/com/fluffyelephant/signatureproblems/Main.java
@@ -0,0 +1,7 @@
+package com.fluffyelephant.signatureproblems;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
